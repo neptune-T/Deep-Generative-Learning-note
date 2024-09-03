397e5864e6db480f66aabb8a85554fc49276d620
plote <2980976495@qq.com>
2024-09-03 19:10:57 +0800

    autoencoder

--- src/autoencoder/autoencoder.py
+++ src/autoencoder/autoencoder.py
@@ -1,12 +1,1 @@
-import torch
-from torchvision import datasets
-from torchvision import transforms
-import matplotlib.pyplot as plt
 
-
-class Autoencoder(nn.Module):
-    def __init__(self):
-        super(Autoencoder,self).__init__()
-        self.encoder = nn.Sequential(
-            nn.linear(28 * 28, 128), 
-        )
